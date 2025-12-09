0b7a25714062e313f4e455a0f0ea6c2c20a03ce7
CH <cedwards2809@gmail.com>
2025-12-09 15:31:53 -0500

    commit

--- src/main.py
+++ src/main.py
@@ -4,27 +4,23 @@ from vex import *
 import math
 
 brain = vex.Brain()
-
 frontLeftMotor = vex.Motor(vex.Ports.PORT8, vex.GearSetting.RATIO_18_1, False)
 frontRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_18_1, True)
 backLeftMotor = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO_18_1, False)
 backRightMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_18_1, True)
 chainSaw = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
-
 controller = vex.Controller(vex.ControllerType.PRIMARY)
+reversed = vex.DirectionType.FORWARD
 
-def xDrive(forward, strafe, turn):
-
+def xDrive(forward, strafe, turn, direction):
     fl = forward + strafe + turn
     fr = forward - strafe - turn
     bl = forward - strafe + turn
     br = forward + strafe - turn
-    
-
-    frontLeftMotor.spin(vex.DirectionType.REVERSE, fl, vex.VelocityUnits.PERCENT)
-    frontRightMotor.spin(vex.DirectionType.REVERSE, fr, vex.VelocityUnits.PERCENT)
-    backLeftMotor.spin(vex.DirectionType.REVERSE, bl, vex.VelocityUnits.PERCENT)
-    backRightMotor.spin(vex.DirectionType.REVERSE, br, vex.VelocityUnits.PERCENT)
+    frontLeftMotor.spin(direction, fl, vex.VelocityUnits.PERCENT)
+    frontRightMotor.spin(direction, fr, vex.VelocityUnits.PERCENT)
+    backLeftMotor.spin(direction, bl, vex.VelocityUnits.PERCENT)
+    backRightMotor.spin(direction, br, vex.VelocityUnits.PERCENT)
 
 def chainsaw(turn):
     chainSaw.spin(vex.DirectionType.REVERSE, turn, vex.VelocityUnits.PERCENT)
@@ -33,18 +29,29 @@ while True:
     forward = controller.axis3.value() 
     strafe = controller.axis4.value()  
     turn = controller.axis1.value()
-    xDrive(forward, strafe, turn)
+    xDrive(forward, strafe, turn, reversed)
     while controller.buttonA.pressing():
-        chainsaw(100)
+        chainsaw(200)
         forward = controller.axis3.value() 
         strafe = controller.axis4.value()  
         turn = controller.axis1.value()
-        xDrive(forward, strafe, turn)
+        xDrive(forward, strafe, turn, reversed)
     while controller.buttonB.pressing():
         chainsaw(-100)
         forward = controller.axis3.value() 
         strafe = controller.axis4.value()  
         turn = controller.axis1.value()
-        xDrive(forward, strafe, turn)
+        xDrive(forward, strafe, turn, reversed)
+    while controller.buttonX.pressing():
+        chainsaw(100)
+        forward = controller.axis3.value() 
+        strafe = controller.axis4.value()  
+        turn = controller.axis1.value()
+        xDrive(forward, strafe, turn, reversed)
+    if controller.buttonX.pressed:
+        if reversed is vex.DirectionType.FORWARD:
+            reversed = vex.DirectionType.REVERSE
+        if reversed is vex.DirectionType.REVERSE:
+            reversed = vex.DirectionType.FORWARD
     chainsaw(0)
     
