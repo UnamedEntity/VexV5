97034d9c29280f6514e105d9f179d107dce6bfad
CH <cedwards2809@gmail.com>
2025-12-12 10:34:42 -0500

    commit

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from vex import *
 import math
 
 brain = vex.Brain()
-frontLeftMotor = vex.Motor(vex.Ports.PORT8, vex.GearSetting.RATIO_18_1, False)
-frontRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_18_1, True)
-backLeftMotor = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO_18_1, False)
-backRightMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_18_1, True)
-chainSaw = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
+frontLeftMotor = vex.Motor(vex.Ports.PORT8, vex.GearSetting.RATIO_36_1, False)
+frontRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_36_1, True)
+backLeftMotor = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO_36_1, False)
+backRightMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_36_1, True)
+chainSaw = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_36_1, True)
 controller = vex.Controller(vex.ControllerType.PRIMARY)
 reversed = vex.DirectionType.FORWARD
 
@@ -38,12 +38,6 @@ while True:
         xDrive(forward, strafe, turn, reversed)
     while controller.buttonB.pressing():
         chainsaw(-100)
-        forward = controller.axis3.value() 
-        strafe = controller.axis4.value()  
-        turn = controller.axis1.value()
-        xDrive(forward, strafe, turn, reversed)
-    while controller.buttonX.pressing():
-        chainsaw(100)
         forward = controller.axis3.value() 
         strafe = controller.axis4.value()  
         turn = controller.axis1.value()
